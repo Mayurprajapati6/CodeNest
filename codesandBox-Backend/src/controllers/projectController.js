02c342846c5d04b47ff4271c792fdcb28b9fff03
Mayur Prajapati <mproyur685@gmail.com>
2025-06-17 22:22:26 +0530

    Connect socket.io to backend

--- codesandBox-Backend/src/controllers/projectController.js
+++ codesandBox-Backend/src/controllers/projectController.js
@@ -2,23 +2,36 @@ import util from 'util';
 import child_process  from 'child_process';
 import fs from 'fs/promises';
 import uuid4 from 'uuid4';
+import { REACT_PROJECT_COMMAND } from '../config/serverConfig.js';
+import { createProjectService, getProjectTreeServices } from '../service/projectService.js';
 
-const execPromisified = util.promisify(child_process.exec);
+//const execPromisified = util.promisify(child_process.exec);
 
 export const createProjectController = async (req,res) => {
 
-    // Create a unique id and then inside the projects folder create a new folder with that id
+    // // Create a unique id and then inside the projects folder create a new folder with that id
 
-    const projectId = uuid4();
-    console.log("New project id is", projectId);
+    // const projectId = uuid4();
+    // console.log("New project id is", projectId);
 
-    await fs.mkdir(`./projects/${projectId}`);
+    // await fs.mkdir(`./projects/${projectId}`);
 
-    // After this call the npm create vite command in the newly created project folder
+    // // After this call the npm create vite command in the newly created project folder
 
-    const response = await execPromisified('npm create vite@latest sandbox -- --template react',{
-        cwd: `./projects/${projectId}`
-    })
+    // const response = await execPromisified(REACT_PROJECT_COMMAND,{
+    //     cwd: `./projects/${projectId}`
+    // })
+
+    const projectId = await createProjectService();
     
     return res.json({ message: 'Project created', data: projectId});
+}
+
+export const getProjectTree = async (req,res) => {
+    const tree = await getProjectTreeServices(req.params.projectId);
+    return res.status(200).json({
+        data: tree,
+        success: true,
+        message: "Successfully fetched the tree"
+    })
 }
--- codesandBox-Backend/src/index.js
+++ codesandBox-Backend/src/index.js
@@ -1,14 +1,27 @@
 import express from 'express';
 import cors from 'cors';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
 import apiRouter from './routes/index.js';
 
 import { PORT } from './config/serverConfig.js';
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server,{
+  cors: {
+    origin: '*',
+    method: ['GET','POST'],
+  }
+});
 
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
 
 app.use('/api',apiRouter);
 
@@ -16,6 +29,6 @@ app.get('/ping',(req,res) => {
     return res.json({ message: 'ping'});
 })
 
-app.listen(PORT,() => {
+server.listen(PORT,() => {
     console.log(`server is running on port ${PORT}`);
 })
